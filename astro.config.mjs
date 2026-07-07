4a542019e43d4cf4ae2e40c8bb7bfb1902ac8523
Vasile Pop <vasile.pop@gmail.com>
2026-07-07 15:33:59 +0300

    add OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://baard.vercel.app',
   vite: {
     plugins: [tailwindcss()],
   },
